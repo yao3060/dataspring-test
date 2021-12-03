47d7062c7405afc268a06eee63172c5067c0e44e
Yao.Yingying <you3060@163.com>
2021-12-03 12:19:11 +0800

    add GetGreetingContent  test

--- tests/MyGreeterClientTest.php
+++ tests/MyGreeterClientTest.php
@@ -2,6 +2,12 @@
 
 use PHPUnit\Framework\TestCase;
 
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+date_default_timezone_set($_ENV['TIMEZONE']);
+
+
 class MyGreeterClientTest extends TestCase
 {
     protected $greeter;
@@ -19,5 +25,12 @@ class MyGreeterClientTest extends TestCase
     public function testGetGreeting()
     {
         $this->assertTrue(strlen($this->greeter->getGreeting()) > 0);
+    }
+
+    public function testGetGreetingContent()
+    {
+        $testArray = array("Good morning",   "Good afternoon", 'Good evening');
+        $value = $this->greeter->getGreeting();
+        $this->assertContains($value, $testArray, "testArray doesn't contains value as value");
     }
 }
